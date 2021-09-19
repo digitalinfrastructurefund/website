6ba86149a0bf709cbdef3862a0aad1247192860e
flickz <omoyajowo2015@gmail.com>
2021-09-19 23:33:17 +0100

    add teamMemeberCard component

---
+++ src/components/TeamMemberCard.jsx
@@ -0,0 +1,24 @@
+import * as React from "react";
+import { Avatar, Text, HStack, VStack } from "@chakra-ui/react";
+
+const TeamMemberCard = ({ name, role, image }) => (
+  <HStack spacing='24px'>
+    <Avatar
+      borderRadius='24px'
+      w='104px'
+      h='104px'
+      src={image.publicURL}
+      alt={name}
+    />
+    <VStack alignItems='flex-start'>
+      <Text textStyle='paragraph-1' color='primaryDarkGrey'>
+        {name}
+      </Text>
+      <Text textStyle='paragraph-2' color='secondaryMidGray'>
+        {role}
+      </Text>
+    </VStack>
+  </HStack>
+);
+
+export default TeamMemberCard;
