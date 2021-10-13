74463014f5a8c52be988e0b5edc4ad10c0b1516b
flickz <omoyajowo2015@gmail.com>
2021-10-13 18:35:43 +0000

    fix build

--- gatsby-config.js
+++ gatsby-config.js
@@ -76,12 +76,5 @@ module.exports = {
         timeout: 3500,
       },
     },
-    {
-      resolve: 'gatsby-plugin-mailchimp',
-      options: {
-          endpoint: '', // string; add your MC list endpoint here; see instructions below
-          timeout: 3500, // number; the amount of time, in milliseconds, that you want to allow mailchimp to respond to your request before timing out. defaults to 3500
-      },
-    },
   ],
 };
