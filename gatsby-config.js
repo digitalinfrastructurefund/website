14921d4bff6e589406cfdfa3898d54637e63e0dc
BenJam <benjam@github.com>
2021-10-13 14:34:19 +0100

    Remove additional participants add stubs for proposals

--- gatsby-config.js
+++ gatsby-config.js
@@ -76,5 +76,12 @@ module.exports = {
         timeout: 3500,
       },
     },
+    {
+      resolve: 'gatsby-plugin-mailchimp',
+      options: {
+          endpoint: '', // string; add your MC list endpoint here; see instructions below
+          timeout: 3500, // number; the amount of time, in milliseconds, that you want to allow mailchimp to respond to your request before timing out. defaults to 3500
+      },
+    },
   ],
 };
